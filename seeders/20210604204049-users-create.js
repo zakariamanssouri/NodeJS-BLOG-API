0791491290aa0870263f590e0b3fe8934d3b29b3
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2021-06-04 22:45:40 +0200

    création de mérhode up pour ajouter dees utilisateurs fictifs

---
+++ seeders/20210604204049-users-create.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const { random, fake } = require('faker');
+const faker = require('faker');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    let data = [];
+    let nombre = 20;
+
+    const roles = ['admin', 'author', 'guest']
+
+    let date = faker.date.past();
+    while (nombre--) {
+      data.push({
+        username: faker.internet.userName,
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        role: faker.helpers.randomize(roles),
+        createdAt: date,
+        updatedAt: date
+      })
+    }
+    console.log(data)
+    await queryInterface.bulkInsert('Users', data, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
