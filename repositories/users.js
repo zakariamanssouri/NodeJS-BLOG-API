7239f683d6450d099bc6847116497592d2fc1ee7
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2021-06-05 00:38:32 +0200

    création du fichier /repositories/users.js et l'ajout des méthodes permettant de gérer les utilisateurs

---
+++ repositories/users.js
@@ -0,0 +1,84 @@
+const { User } = require('../models')
+
+module.exports = {
+
+    getAllUsers() {
+        return User.findAll()
+    },
+
+    getUsers(offset = 0, limit = 10) {
+        return User.finAndCountAll({
+            limit: limit,
+            offset: offset,
+            order: [['createdAt', 'DESC']]
+        })
+    },
+    getAdmins() {
+
+        return User.findAll({
+            where: {
+                role: "admin"
+            }
+        })
+    },
+
+    getAuthors() {
+        return User.findAll({
+            where: {
+                role: "author"
+            }
+        }
+        )
+    },
+
+    getGuests() {
+        return User.findAll({
+            where: {
+                role: "guest"
+            }
+        }
+
+        )
+    },
+
+    getUser(id) {
+        return User.finAll({
+            where: {
+                id: id
+            }
+        }
+        )
+    },
+
+    getUserByEmail(email) {
+        return User.findOne({
+            where: {
+                email: email
+            }
+        }
+
+        )
+    },
+    addUser(user) {
+        return User.create(user)
+    },
+
+    updateUser(user) {
+        return User.update(user,
+            {
+                where: {
+                    id: user.id
+                }
+            })
+    },
+
+    deleteUser(id) {
+        return User.destroy(
+            {
+                where: {
+                    id: id
+                }
+            }
+        )
+    }
+}
